3301c088e34333da9262e6e877a9a33b167d8c33
Imran Khan <emrankhanniloy@gmail.com>
2025-12-11 12:34:01 +0600

    fix: deploy issue

--- src/app/(turfProfile)/[turfProfileSlug]/(turfUserAuth)/turf-user/login/page.tsx
+++ src/app/(turfProfile)/[turfProfileSlug]/(turfUserAuth)/turf-user/login/page.tsx
@@ -1,15 +1,41 @@
+// import TurfUserLoginForm from "@/components/modules/TurfUser/TurfUserLoginForm";
+
+// const TurfUserLoginPage = async ({
+//   params,
+//   searchParams,
+// }: {
+//   params: { turfProfileSlug: string }
+//   searchParams?: Promise<{ redirect?: string }>;
+// }) => {
+//   const qs = (await searchParams) || {};
+//   const resolvedParams = await params;
+
+//   return (
+//     <div className="flex min-h-screen items-center justify-center">
+//       <div className="w-full max-w-md space-y-6 rounded-lg border p-8 shadow-lg">
+//         <div className="space-y-2 text-center">
+//           <h1 className="text-3xl font-bold">Welcome Back, Login as Turf User</h1>
+//           <p className="text-gray-500">
+//             Enter your credentials to access your account
+//           </p>
+//         </div>
+
+//         {/* Pass turfPublicId to form */}
+//         <TurfUserLoginForm
+//           redirect={qs.redirect}
+//           turfProfileSlug={resolvedParams.turfProfileSlug}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default TurfUserLoginPage;
+
 import TurfUserLoginForm from "@/components/modules/TurfUser/TurfUserLoginForm";
 
-const TurfUserLoginPage = async ({
-  params,
-  searchParams,
-}: {
-  params: { turfProfileSlug: string };
-  searchParams?: Promise<{ redirect?: string }>;
-}) => {
-  const qs = (await searchParams) || {};
-  const resolvedParams = await params;
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const TurfUserLoginPage = ({ params, searchParams }: any) => {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="w-full max-w-md space-y-6 rounded-lg border p-8 shadow-lg">
@@ -20,10 +46,9 @@ const TurfUserLoginPage = async ({
           </p>
         </div>
 
-        {/* Pass turfPublicId to form */}
         <TurfUserLoginForm
-          redirect={qs.redirect}
-          turfProfileSlug={resolvedParams.turfProfileSlug}
+          redirect={searchParams?.redirect}
+          turfProfileSlug={params.turfProfileSlug}
         />
       </div>
     </div>
