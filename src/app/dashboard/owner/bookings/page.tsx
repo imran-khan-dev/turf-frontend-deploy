a6b1ef8184ae1cb613d3006445bcc17117cf970b
Imran Khan <emrankhanniloy@gmail.com>
2025-12-11 20:39:37 +0600

    add: ownerBookingTable

--- src/app/dashboard/owner/bookings/page.tsx
+++ src/app/dashboard/owner/bookings/page.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import BookingsTable from "@/components/modules/Booking/BookingsTable";
+import BookingsTableOwner from "@/components/modules/Booking/BookingsTableOwner";
 import serverFetch from "@/lib/server-fetch";
 
 export default async function OwnerBookingsPage() {
@@ -21,7 +21,7 @@ export default async function OwnerBookingsPage() {
   return (
     <div className="rounded-xl border border-[#1A80E3]/30 shadow-lg p-4">
       <h2 className="text-xl font-semibold mb-4 text-[#1A80E3]">All Bookings</h2>
-      <BookingsTable initialBookings={bookings} />
+      <BookingsTableOwner initialBookings={bookings} />
     </div>
   );
 }
